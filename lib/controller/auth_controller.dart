fb3f05552df6cf309ef2de5a722ce4bdd255fa35
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-08-08 22:36:47 +0630

    create the auth-controller

---
+++ lib/controller/auth_controller.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_fiberbase_login/Splash/splash.dart';
+import 'package:flutter_fiberbase_login/login/login.dart';
+import 'package:get/get.dart';
+
+class AuthController extends GetxController {
+  static AuthController instance = Get.find();
+  late Rx<User?> _user;
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  @override
+  void onReady() {
+    _user = Rx<User?>(auth.currentUser);
+    _user.bindStream(auth.userChanges());
+    ever(_user, _initialScreen);
+  }
+
+  _initialScreen(User? user) {
+    if (user == null) {
+      print("Login Page");
+      Get.offAll(() => LoginPage());
+    } else {
+      Get.offAll(() => Splash());
+    }
+  }
+
+  void register(String email, password) {
+    try {
+      auth.createUserWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      Get.snackbar("About User", "User message",
+          backgroundColor: Colors.redAccent,
+          snackPosition: SnackPosition.BOTTOM,
+          titleText: Text(
+            "Account Creation failed",
+            style: TextStyle(color: Colors.white),
+          ),
+          messageText: Text(
+            e.toString(),
+            style: TextStyle(color: Colors.white),
+          ));
+    }
+  }
+}
